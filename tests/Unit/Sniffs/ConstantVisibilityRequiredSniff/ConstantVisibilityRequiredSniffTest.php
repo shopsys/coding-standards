yaninnaa/shopsys

added sniff for required visibility for constants
- constant have to have visibility modifier (preferred) or access annotation (deprecated)
- sniff is not yet enabled nor used

packages/coding-standards/tests/Unit/Sniffs/ConstantVisibilityRequiredSniff/ConstantVisibilityRequiredSniffTest.php
@@ -1,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\CodingStandards\Sniffs\ConstantVisibilityRequiredSniff;
+
+use Symplify\EasyCodingStandardTester\Testing\AbstractCheckerTestCase;
+
+final class ConstantVisibilityRequiredSniffTest extends AbstractCheckerTestCase
+{
+    public function testCorrect(): void
+    {
+        $this->doTestCorrectFile(__DIR__ . '/Correct/Annotation.php');
+        $this->doTestCorrectFile(__DIR__ . '/Correct/SingleValueWithoutNamespace.php');
+        $this->doTestCorrectFile(__DIR__ . '/Correct/SingleValueAfterMethodWithoutNamespace.php');
+        $this->doTestCorrectFile(__DIR__ . '/Correct/MultipleValues.php');
+        $this->doTestCorrectFile(__DIR__ . '/Correct/Mixed.php');
+        $this->doTestCorrectFile(__DIR__ . '/Correct/noClass.php');
+        $this->doTestCorrectFile(__DIR__ . '/Correct/OutsideClass.php');
+    }
+
+    public function testWrong(): void
+    {
+        $this->doTestWrongFile(__DIR__ . '/Wrong/SingleValue.php');
+        $this->doTestWrongFile(__DIR__ . '/Wrong/MissingAnnotation.php');
+        $this->doTestWrongFile(__DIR__ . '/Wrong/Mixed.php');
+        $this->doTestWrongFile(__DIR__ . '/Wrong/MixedAtTheEnd.php');
+        $this->doTestWrongFile(__DIR__ . '/Wrong/MixedInTheMiddle.php');
+        $this->doTestWrongFile(__DIR__ . '/Wrong/SingleValueAfterMethodWithoutNamespaceWrong.php');
+    }
+
+    /**
+     * @return string
+     */
+    protected function provideConfig(): string
+    {
+        return __DIR__ . '/config.yml';
+    }
+}

shopsys/coding-standards

tests/Unit/Sniffs/ConstantVisibilityRequiredSniff/ConstantVisibilityRequiredSniffTest.php
@@ -1,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\CodingStandards\Sniffs\ConstantVisibilityRequiredSniff;
+
+use Symplify\EasyCodingStandardTester\Testing\AbstractCheckerTestCase;
+
+final class ConstantVisibilityRequiredSniffTest extends AbstractCheckerTestCase
+{
+    public function testCorrect(): void
+    {
+        $this->doTestCorrectFile(__DIR__ . '/correct/Annotation.php');
+        $this->doTestCorrectFile(__DIR__ . '/correct/SingleValueWithoutNamespace.php');
+        $this->doTestCorrectFile(__DIR__ . '/correct/SingleValueAfterMethodWithoutNamespace.php');
+        $this->doTestCorrectFile(__DIR__ . '/correct/MultipleValues.php');
+        $this->doTestCorrectFile(__DIR__ . '/correct/Mixed.php');
+        $this->doTestCorrectFile(__DIR__ . '/correct/MixedVisibilities.php');
+        $this->doTestCorrectFile(__DIR__ . '/correct/noClass.php');
+        $this->doTestCorrectFile(__DIR__ . '/correct/OutsideClass.php');
+    }
+
+    public function testWrong(): void
+    {
+        $this->doTestWrongFile(__DIR__ . '/wrong/SingleValue.php');
+        $this->doTestWrongFile(__DIR__ . '/wrong/MissingAnnotation.php');
+        $this->doTestWrongFile(__DIR__ . '/wrong/Mixed.php');
+        $this->doTestWrongFile(__DIR__ . '/wrong/MixedAtTheEnd.php');
+        $this->doTestWrongFile(__DIR__ . '/wrong/MixedInTheMiddle.php');
+        $this->doTestWrongFile(__DIR__ . '/wrong/SingleValueAfterMethodWithoutNamespace.php');
+    }
+
+    /**
+     * @return string
+     */
+    protected function provideConfig(): string
+    {
+        return __DIR__ . '/config.yml';
+    }
+}
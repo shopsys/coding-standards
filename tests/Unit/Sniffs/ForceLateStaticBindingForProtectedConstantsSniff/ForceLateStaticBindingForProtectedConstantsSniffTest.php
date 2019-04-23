yaninnaa/shopsys

added fixer forcing late static binding for protected constants

packages/coding-standards/tests/Unit/Sniffs/ForceLateStaticBindingForProtectedConstantsSniff/ForceLateStaticBindingForProtectedConstantsSniffTest.php
@@ -1,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\CodingStandards\Sniffs\ForceLateStaticBindingForProtectedConstantsSniff;
+
+use Symplify\EasyCodingStandardTester\Testing\AbstractCheckerTestCase;
+
+final class ForceLateStaticBindingForProtectedConstantsSniffTest extends AbstractCheckerTestCase
+{
+    public function testFix(): void
+    {
+        $this->doTestWrongToFixedFile(__DIR__ . '/Wrong/SingleValue.php', __DIR__ . '/Fixed/SingleValue.php');
+    }
+
+    /**
+     * @return string
+     */
+    protected function provideConfig(): string
+    {
+        return __DIR__ . '/config.yml';
+    }
+}

shopsys/coding-standards

tests/Unit/Sniffs/ForceLateStaticBindingForProtectedConstantsSniff/ForceLateStaticBindingForProtectedConstantsSniffTest.php
@@ -1,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\CodingStandards\Sniffs\ForceLateStaticBindingForProtectedConstantsSniff;
+
+use Symplify\EasyCodingStandardTester\Testing\AbstractCheckerTestCase;
+
+final class ForceLateStaticBindingForProtectedConstantsSniffTest extends AbstractCheckerTestCase
+{
+    public function testFix(): void
+    {
+        $this->doTestWrongToFixedFile(__DIR__ . '/wrong/SingleValue.php', __DIR__ . '/fixed/SingleValue.php');
+        $this->doTestWrongToFixedFile(__DIR__ . '/wrong/SelfWithMethodsAndVariables.php', __DIR__ . '/fixed/SelfWithMethodsAndVariables.php');
+    }
+
+    /**
+     * @return string
+     */
+    protected function provideConfig(): string
+    {
+        return __DIR__ . '/config.yml';
+    }
+}
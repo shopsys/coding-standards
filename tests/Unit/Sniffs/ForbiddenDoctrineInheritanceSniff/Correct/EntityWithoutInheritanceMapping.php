696625fa46969b62831a10ddb6dfe4f61186a2c6
Rostislav Vitek <rostislav.vitek@shopsys.com>
2026-04-10 15:40:03 +0200

    mcp: replaced implicit schema exposure with explicit attrs
    - adds #[AsMcpTable] and #[AsMcpColumn] as the explicit MCP exposure contract
    - removes the old inferred queryable/blacklisted attribute model
    - wires MCP-owned PHPStan enforcement and package dependencies for the new attrs

--- tests/Unit/Sniffs/ForbiddenDoctrineInheritanceSniff/Correct/EntityWithoutInheritanceMapping.php
+++ tests/Unit/Sniffs/ForbiddenDoctrineInheritanceSniff/Correct/EntityWithoutInheritanceMapping.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Tests\CodingStandards\Unit\Sniffs\ForbiddenDoctrineInheritanceSniff\Correct;
 
 use Doctrine\ORM\Mapping as ORM;
-
 #[ORM\Entity]
 class EntityWithoutInheritanceMapping
 {
--- tests/Unit/Sniffs/ForbiddenDoctrineInheritanceSniff/Wrong/EntityWithOrmInheritanceMapping.php
+++ tests/Unit/Sniffs/ForbiddenDoctrineInheritanceSniff/Wrong/EntityWithOrmInheritanceMapping.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Tests\CodingStandards\Unit\Sniffs\ForbiddenDoctrineInheritanceSniff\Wrong;
 
 use Doctrine\ORM\Mapping as ORM;
-
 #[ORM\Entity]
 #[ORM\InheritanceType('SINGLE_TABLE')]
 class EntityWithOrmInheritanceMapping
